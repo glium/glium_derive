37bf9ca077a6ff827b1ba6b173646e50e3323bb5
Nikolay Arhipov <n@arhipov.net>
2023-04-30 20:28:02 +0300

    Implemented Uniforms derive macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! A custom derive implementation for Glium Vertex
+//! A custom derive implementation for Glium Vertex and Uniforms traits
 //!
 //! ## Example
 //! ```rust
@@ -14,6 +14,15 @@
 //!     uv: [f32; 2],
 //!     #[glium(attr = "a_color", normalize)]
 //!     color: u32,
+//!     a_custom: u32, // the attribute can be omitted
+//! }
+//!
+//! //! #[derive(Clone, Copy, Uniforms)]
+//! struct MyUniforms {
+//!     #[glium(attr = "a_view")]
+//!     view: [[f32; 4]; 4],
+//!     #[glium(attr = "a_color")]
+//!     color: [f32; 4],
 //!     a_custom: u32, // the attribute can be omitted
 //! }
 //! ```
@@ -146,6 +155,91 @@ fn impl_glium_vertex_derive(ast: &DeriveInput) -> TokenStream {
             #[inline]
             fn build_bindings() -> ::glium::vertex::VertexFormat {
                 Self::BINDINGS
+            }
+        }
+    };
+
+    stream.into()
+}
+
+#[proc_macro_derive(Uniforms, attributes(glium))]
+pub fn glium_uniforms_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_glium_uniforms_derive(&ast)
+}
+
+fn impl_glium_uniforms_derive(ast: &DeriveInput) -> TokenStream {
+    let struct_name = &ast.ident;
+    let generics = &ast.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let fields = match ast.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(ref fields),
+            ..
+        }) => {
+            &fields.named
+        },
+        _ => {
+            panic!("#[derive(Uniforms)] only defined for structs.");
+        }
+    };
+
+    let bindings = fields.iter().map(|field| {
+        let attrs = field.attrs.iter()
+            .flat_map(Attribute::parse_meta)
+            .flat_map(|meta| {
+                match meta {
+                    Meta::List(MetaList { ref path, ref nested, .. }) => {
+                        let ident = path.get_ident().expect("Expected ident got path");
+
+                        if ident == "glium" {
+                            nested.iter().cloned().collect()
+                        } else {
+                            Vec::new()
+                        }
+                    },
+                    _ => {
+                        Vec::new()
+                    }
+                }
+            });
+
+        let field_name = &field.ident;
+        let uniform_lit = format!("{}", field_name.as_ref().unwrap());
+        let mut uniform_name = quote!(#uniform_lit);
+
+        for meta in attrs {
+            match meta {
+                NestedMeta::Meta(Meta::NameValue(MetaNameValue { ref path, ref lit, .. })) => {
+                    let ident = path.get_ident().expect("Expected ident got path");
+
+                    if ident == "attr" {
+                        uniform_name = quote!(#lit);
+                    } else {
+                        panic!("Unknown field attribute {}", ident);
+                    }
+                },
+                NestedMeta::Meta(Meta::Path(ref path)) => {
+                    let ident = path.get_ident().expect("Expected ident got path");
+                    panic!("Unknown field attribute {}", ident);
+                },
+                _ => (),
+            }
+        };
+
+        quote_spanned! {field.span()=>
+            output(#uniform_name, ::glium::uniforms::AsUniformValue::as_uniform_value(&self.#field_name));
+        }
+    });
+
+    let stream = quote! {
+        impl #impl_generics ::glium::uniforms::Uniforms for #struct_name #ty_generics #where_clause {
+            #[inline]
+            fn visit_values<'a, F: FnMut(&str, ::glium::uniforms::UniformValue<'a>)>(&'a self, mut output: F) {
+                #(
+                    #bindings
+                )*
             }
         }
     };
